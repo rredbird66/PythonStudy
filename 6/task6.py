97638595618ff0f2945c4137dd183dd446d9e145
redbird66 <r3dbird66@yandex.ru>
2021-11-06 04:06:19 +0300

    Task6 done

---
+++ 6/task6.py
@@ -0,0 +1,40 @@
+import argparse
+import json
+import os
+
+from collections import defaultdict
+
+storage_file = "storage.data"
+
+#print("--------Task6--------")
+parser = argparse.ArgumentParser(description='Key-value storage.')
+parser.add_argument('-k', '--key', type = str, help = 'print the values corresponding to the key')
+parser.add_argument('-v', '--val', type = str, help = 'value to save in storage')
+args = parser.parse_args()
+
+if args.key != None and args.val == None:
+    read_file = open(storage_file,"r")
+    data = json.load(read_file)
+    if args.key in data:
+        print(*data[args.key], sep = ', ' )
+    else:
+        print(None)
+    read_file.close();
+if args.key != None and args.val != None:
+    write_file = open(storage_file, "a+")
+    if os.path.getsize(storage_file) == 0:
+        data = defaultdict(list)
+        data[args.key].append(args.val)
+        json.dump(data, write_file)
+    else:
+        write_file.seek(0)
+        data = json.load(write_file)
+        if args.key in data:
+            data[args.key].append(args.val)
+        else:
+            data[args.key] = list()
+            data[args.key].append(args.val)
+        write_file.close()
+        write_file = open(storage_file, "w")
+        json.dump(data, write_file)
+    write_file.close()
