995367c6f77ac300f186bbd0ecb2df7e788b3e92
redbird66 <r3dbird66@yandex.ru>
2021-11-20 09:12:17 +0300

    Update Task3

--- Pack2/3/task3.py
+++ Pack2/3/task3.py
@@ -30,6 +30,26 @@ class Logger(metaclass=Singleton):
         str_to_log = '[{}] {}: {}'.format(level.name, datetime.datetime.now().time(), Msg) + '\n'
         self.filedescr.write(str_to_log)
 
+    def Debug(self, Msg):
+        str_to_log = '[{}] {}: {}'.format(logLevel.DEBUG.name, datetime.datetime.now().time(), Msg) + '\n'
+        self.filedescr.write(str_to_log)
+
+    def Info(self, Msg):
+        str_to_log = '[{}] {}: {}'.format(logLevel.INFO.name, datetime.datetime.now().time(), Msg) + '\n'
+        self.filedescr.write(str_to_log)
+    
+    def Warning(self, Msg):
+        str_to_log = '[{}] {}: {}'.format(logLevel.WARNING.name, datetime.datetime.now().time(), Msg) + '\n'
+        self.filedescr.write(str_to_log)
+
+    def Error(self, Msg):
+        str_to_log = '[{}] {}: {}'.format(logLevel.ERROR.name, datetime.datetime.now().time(), Msg) + '\n'
+        self.filedescr.write(str_to_log)
+
+    def Critical(self, Msg):
+        str_to_log = '[{}] {}: {}'.format(logLevel.CRITICAL.name, datetime.datetime.now().time(), Msg) + '\n'
+        self.filedescr.write(str_to_log)
+
 if __name__ == "__main__":
 
     logger = Logger("out.log")
@@ -44,4 +64,10 @@ if __name__ == "__main__":
     same_log.log(logLevel.ERROR, "Log msg 6")  
     same_log.log(logLevel.WARNING, "Log msg 7")
     same_log.log(logLevel.INFO, "Log msg 8")
- 
+
+    same_log.Debug("Log msg 9")
+    same_log.Error("Log msg 10")  
+    same_log.Warning("Log msg 11")
+    same_log.Info("Log msg 12")
+
+
