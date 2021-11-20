2f76edf84f0eda30e55842ad3f8089009ce03b11
redbird66 <r3dbird66@yandex.ru>
2021-11-20 07:57:12 +0300

    Task2 done

---
+++ Pack2/2/task2.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python3
+
+import math
+
+
+class BaseF:
+    def square(self):
+        raise NotImplementedError()
+
+class Rectangle(BaseF) :
+    name = "Rectangle"
+    def __init__(self, l, h):
+        self.l = l
+        self.h = h
+    def square(self):
+        return self.l * self.h
+
+class Triangle(BaseF) :
+    name = "Triangle"
+    def __init__(self, h, hside):
+        self.h = h
+        self.hside = hside
+    def square(self):
+        return (self.h * self.hside) / 2
+
+class Circle(BaseF) :
+    name = "Circle"
+    def __init__(self, radius):
+        self.radius = radius
+    def square(self):
+        return (math.pi * ( self.radius ** 2 ))
+
+if __name__ == "__main__":
+    
+    a = Rectangle(3,4)
+    print(a.name, a.square())
+    
+    b = Triangle(3,4)
+    print(b.name, b.square())
+
+    c = Circle(5)
+    print(c.name, c.square())
+
+
